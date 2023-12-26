246d3ea0aa35caca9ce0dd9392f55321ca815ba7
riskidwi11 <riskidwipatrio1711@gmail.com>
2023-12-26 14:29:21 +0700

    update table

--- resources/js/Pages/Admin/Master/PaketCategory/Table/PaketCategoryTable.jsx
+++ resources/js/Pages/Admin/Master/PaketCategory/Table/PaketCategoryTable.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import { DataGrid } from "@mui/x-data-grid";
+import { createTheme, ThemeProvider } from "@mui/material";    
 import { CrudButton } from "@/App/Components/Button";
 import useCrud from "@/App/Utils/hooks/useCrud";
 import { useSelector } from "react-redux";
 export default function PaketCategoryTable({ category }) {
     const { handleDelete } = useCrud("master.paket_kategori");
     const loading = useSelector((state) => state.page.showMiniLoading);
+
+    const theme = createTheme({
+        typography: {
+          fontFamily: 'poppins',
+          body1: {
+            padding: 0,
+            margin: 0
+          }
+        }
+      });
 
     const columns = [
         { field: "title", headerName: "Nama Kategori", flex: 3 },
@@ -31,5 +42,7 @@ export default function PaketCategoryTable({ category }) {
         },
     ];
 
-    return <DataGrid editMode="row" rows={category} columns={columns} />;
+    return <ThemeProvider theme={theme}>
+        <DataGrid className="DataTable" editMode="row" rows={category} columns={columns} />
+    </ThemeProvider>;
 }
